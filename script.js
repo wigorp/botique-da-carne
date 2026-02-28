66ca83be857ee1907046723767ea47cfdef923fb
wigorp <55330643+wigorp@users.noreply.github.com>
2026-02-28 01:38:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const numero="5562992819373";
 
-const categoriasDiv=document.getElementById("categorias");
 const produtosDiv=document.getElementById("produtos");
 const itensDiv=document.getElementById("itens");
 const totalDiv=document.getElementById("total");
@@ -13,37 +12,18 @@ const telefone=document.getElementById("telefone");
 const endereco=document.getElementById("endereco");
 
 let carrinho={};
-let categoriaAtual="";
-
-/* PRODUTOS */
 
 const produtos=[
-{cat:"Bovinos",nome:"Contra Filé",preco:60,img:"img/logo.png"},
-{cat:"Bovinos",nome:"Picanha",preco:95,img:"img/logo.png"},
-{cat:"Frango",nome:"Frango Bacon",preco:50,img:"img/logo.png"}
+{nome:"Contra Filé",preco:60,img:"img/logo.png"},
+{nome:"Picanha",preco:95,img:"img/logo.png"},
+{nome:"Frango Bacon",preco:50,img:"img/logo.png"}
 ];
 
-/* CATEGORIAS */
+function mostrar(){
 
-const categorias=[...new Set(produtos.map(p=>p.cat))];
-
-categorias.forEach(cat=>{
-categoriasDiv.innerHTML+=`
-<button onclick="mostrar('${cat}')">
-${cat}
-</button>`;
-});
-
-/* MOSTRAR */
-
-function mostrar(cat){
-
-categoriaAtual=cat;
 produtosDiv.innerHTML="";
 
 produtos.forEach((p,i)=>{
-
-if(p.cat!==cat)return;
 
 produtosDiv.innerHTML+=`
 <div class="card"
@@ -56,12 +36,9 @@ style="background-image:url('${p.img}')">
 ${carrinho[i]||0}
 <button onclick="alterar(${i},1)">+</button>
 
-</div>
-`;
+</div>`;
 });
 }
-
-/* ALTERAR */
 
 function alterar(i,v){
 
@@ -71,10 +48,8 @@ if(carrinho[i]<=0)
 delete carrinho[i];
 
 render();
-mostrar(categoriaAtual);
+mostrar();
 }
-
-/* RENDER */
 
 function render(){
 
@@ -83,30 +58,24 @@ let html="";
 
 Object.keys(carrinho).forEach(i=>{
 
-const p=produtos[i];
-const qtd=carrinho[i];
-const sub=p.preco*qtd;
+let p=produtos[i];
+let qtd=carrinho[i];
+let sub=p.preco*qtd;
 
 totalProdutos+=sub;
 
-html+=`
-${p.nome} x${qtd}
-<br>
-R$ ${sub.toFixed(2)}
-<hr>
-`;
+html+=`${p.nome} x${qtd}<br>`;
 });
 
-const frete=Number(bairro.value||0);
-const totalFinal=totalProdutos+frete;
+let frete=Number(bairro.value||0);
+let total=totalProdutos+frete;
 
-itensDiv.innerHTML=html || "Nenhum item";
+itensDiv.innerHTML=html||"Nenhum item";
 
-totalDiv.innerHTML=`
-Produtos: R$ ${totalProdutos.toFixed(2)}<br>
-Frete: R$ ${frete.toFixed(2)}<br>
-<b>Total: R$ ${totalFinal.toFixed(2)}</b>
-`;
+totalDiv.innerHTML=
+`Produtos: R$${totalProdutos.toFixed(2)}<br>
+Frete: R$${frete.toFixed(2)}<br>
+<b>Total: R$${total.toFixed(2)}</b>`;
 }
 
 bairro.addEventListener("change",render);
@@ -117,100 +86,48 @@ function enviarPedido(){
 
 let msg="Pedido Botique da Carne\n\n";
 
-let totalProdutos=0;
-
 Object.keys(carrinho).forEach(i=>{
-
-const p=produtos[i];
-const qtd=carrinho[i];
-const sub=p.preco*qtd;
-
-msg+=`${p.nome} x${qtd} - R$${sub.toFixed(2)}\n`;
-totalProdutos+=sub;
+msg+=`${produtos[i].nome} x${carrinho[i]}\n`;
 });
-
-const frete=Number(bairro.value||0);
-const totalFinal=totalProdutos+frete;
-
-msg+=`\nFrete: R$${frete.toFixed(2)}`;
-msg+=`\nTotal: R$${totalFinal.toFixed(2)}\n\n`;
-
-msg+=`Cliente: ${nome.value}`;
-msg+=`\nTelefone: ${telefone.value}`;
-msg+=`\nEndereço: ${endereco.value}`;
-msg+=`\nPagamento: ${pagamento.value}`;
 
 window.open(
 `https://wa.me/${numero}?text=${encodeURIComponent(msg)}`
 );
 }
 
-/* IMPRESSÃO SEM CRASH */
+/* IMPRESSÃO SUNMI SAFE */
 
 function imprimirPedido(){
 
-let html=`
-<center>
-<img src="img/logo.png" width="120"><br>
-<b>Botique da Carne</b>
-<hr>
-</center>
-`;
-
-let totalProdutos=0;
+let html=`<center>
+<img src="img/logo.png" width="100"><hr>`;
 
 Object.keys(carrinho).forEach(i=>{
-
-const p=produtos[i];
-const qtd=carrinho[i];
-const sub=p.preco*qtd;
-
-totalProdutos+=sub;
-
-html+=`
-${p.nome}<br>
-${qtd} x ${p.preco.toFixed(2)}<br><br>
-`;
+html+=`${produtos[i].nome} x${carrinho[i]}<br>`;
 });
 
-const frete=Number(bairro.value||0);
-const totalFinal=totalProdutos+frete;
-
-html+=`
-<hr>
-Produtos: R$ ${totalProdutos.toFixed(2)}<br>
-Frete: R$ ${frete.toFixed(2)}<br>
-<b>Total: R$ ${totalFinal.toFixed(2)}</b>
-<hr>
-
-Cliente: ${nome.value}<br>
-Tel: ${telefone.value}<br>
-End: ${endereco.value}<br>
-Pag: ${pagamento.value}
-
-<br><br><br>
-<center>Obrigado!</center>
-`;
+html+=`<hr>
+Cliente:${nome.value}<br>
+Tel:${telefone.value}<br>
+End:${endereco.value}
+</center>`;
 
 const area=document.getElementById("printArea");
 
 area.innerHTML=html;
-area.style.display="block";
 
 setTimeout(()=>{
 window.print();
-area.style.display="none";
-},300);
+},200);
+
 }
 
-/* TELEFONE 11 DÍGITOS */
+/* TELEFONE 11 DIGITOS */
 
 telefone.addEventListener("input",()=>{
 telefone.value=
 telefone.value.replace(/\D/g,"").slice(0,11);
 });
 
-/* INIT */
-
-mostrar(categorias[0]);
+mostrar();
 render();
